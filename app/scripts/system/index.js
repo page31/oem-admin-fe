55a31d76e835c9202b00469641bf789ca3d865f5
gaohailang <ghlndsl@126.com>
2014-11-27 20:43:56 +0800

    feat: add basic system related formly config fields

--- app/scripts/system/index.js
+++ app/scripts/system/index.js
@@ -4,7 +4,13 @@ define(['base/index'], function() {
     /* Routers defined here */
     // require/define 的区别 - 导致dependence 未 ready
     // Fuck config 不能配置 factory!!
-    systemApp.config(function($routeHelperProvider) {
+    systemApp.config(function($routeHelperProvider, $stateProvider) {
+        $stateProvider.state('system', {
+            url: '/system',
+            templateUrl: 'templates/system/index.html',
+            controller: 'systemCtrl'
+        });
+
         var systemMenuList = [{
             name: '管理 OEM / API',
             id: 'partner'
@@ -30,9 +36,75 @@ define(['base/index'], function() {
     }
 
     /* Controllers defined here */
-    systemApp.controller('systemCtrl', function($scope) {
+    systemApp.controller('systemCtrl', function($scope, formlyHelper) {
         // extract to i18n
         $scope.menuList = systemMenuList;
+
+        var channelForm = {
+            modalTitle: '添加 OEM 渠道',
+            formFields: [{
+                label: '渠道名称',
+                key: 'channelName',
+                placeholder: '例如， 联想 K900'
+            }, {
+                label: '渠道 User Source',
+                key: 'channelSource',
+                placeholder: '例如， lenovo_K900'
+            }]
+        };
+
+        var apiPartnerForm = {
+            modalTitle: '添加 API 合作方',
+            formFields: [{
+                label: 'API 合作方名称',
+                key: 'hzfcmc1',
+                placeholder: '例如：赶集网'
+            }, {
+                label: 'API 合作方授权 ID',
+                key: 'hzfcmc2',
+                placeholder: '例如：赶集网'
+            }, {
+                label: 'API 方下载豌豆荚 APK  包的 URL (可选)',
+                key: 'hzfcmc3',
+                placeholder: '用于追踪 API 合作方为豌豆荚这款应用带来的装机量'
+            }, {
+                label: '优质应用变更时通知合作方的 API 地址（可选）',
+                key: 'hzfcmc4',
+                placeholder: '由合作方提供，在优质应用变更时调用'
+            }, {
+                label: '接口访问频率权限',
+                key: 'hzfcmc5',
+                optionStr: 'idx as opt for (idx, opt) in options.options',
+                options: ['高级授权', '中级授权', '低级授权']
+            }, {
+                label: '开放的接口',
+                key: 'hzfcmc',
+                placeholder: '例如：赶集网'
+            }] // Todo: add multi checkbox support
+        };
+
+        // Todo: with formName: <账户>, type: add/modify to compute modalTitle
+        // Todo: 根据 role 的取值，需要切换 formFields 内容！ - accountPartnerForm
+        var accountForm = {
+            modalTitle: '添加新账户',
+            formFields: [{
+                label: '豌豆荚账户',
+                key: 'wdjAccount'
+            }, {
+                label: '账户角色',
+                key: 'role',
+                optionStr: 'idx as opt for (idx, opt) in options.options',
+                options: ['内部运营', '外部运营', '厂商工作人员']
+            }, {
+                label: '可管理的 OEM/API',
+                controlTpl: '' // 异步接口数据，自定义 checkbox
+            }, {
+                label: '开设权限',
+                controlTpl: ''
+            }]
+        };
+
+        formlyHelper.openModal(apiPartnerForm);
     });
 
     systemApp.controller('systemPartnerCtrl', function($scope) {
