773b94d58f20fc69bb5cd9ebb03b754a3f34e87c
gaohailang <ghlndsl@126.com>
2014-12-06 16:38:47 +0800

    feat: simply use backend api to partners(api, oem) etc and and controlTpl support (for oemPartner and channels~)

--- app/scripts/system/index.js
+++ app/scripts/system/index.js
@@ -40,97 +40,124 @@ define(['base/index'], function() {
         // extract to i18n
         $scope.menuList = systemMenuList;
 
-        var channelForm = {
-            modalTitle: '添加 OEM 渠道',
-            formFields: [{
-                label: '渠道名称',
-                key: 'channelName',
-                placeholder: '例如， 联想 K900'
-            }, {
-                label: '渠道 User Source',
-                key: 'channelSource',
-                placeholder: '例如， lenovo_K900'
-            }]
+        var FORMMAP = {
+            channel: {
+                modalTitle: '添加 OEM 渠道',
+                formFields: [{
+                    label: '渠道名称',
+                    key: 'channelName',
+                    placeholder: '例如， 联想 K900'
+                }, {
+                    label: '渠道 User Source',
+                    key: 'channelSource',
+                    placeholder: '例如， lenovo_K900'
+                }]
+            },
+            oemPartner: {
+                modalTitle: '添加 OEM 合作方',
+                formFields: [{
+                    label: '合作方名称',
+                    key: 'channelName',
+                    placeholder: '例如， 联想'
+                }, {
+                    label: 'OEM 合作方 ID',
+                    key: 'channelSource',
+                    placeholder: '例如， lenovo'
+                }, {
+                    label: 'OEM 渠道',
+                    controlTpl: 'system/channel-list-snippet.html'
+                }]
+            },
+            apiPartner: {
+                modalTitle: '添加 API 合作方',
+                formFields: [{
+                    label: 'API 合作方名称',
+                    key: 'hzfcmc1',
+                    placeholder: '例如：赶集网'
+                }, {
+                    label: 'API 合作方授权 ID',
+                    key: 'hzfcmc2',
+                    placeholder: '例如：赶集网'
+                }, {
+                    label: 'API 方下载豌豆荚 APK  包的 URL (可选)',
+                    key: 'hzfcmc3',
+                    placeholder: '用于追踪 API 合作方为豌豆荚这款应用带来的装机量'
+                }, {
+                    label: '优质应用变更时通知合作方的 API 地址（可选）',
+                    key: 'hzfcmc4',
+                    placeholder: '由合作方提供，在优质应用变更时调用'
+                }, {
+                    label: '接口访问频率权限',
+                    key: 'hzfcmc5',
+                    optionStr: 'idx as opt for (idx, opt) in options.options',
+                    options: ['高级授权', '中级授权', '低级授权']
+                }, {
+                    label: '开放的接口',
+                    key: 'hzfcmc',
+                    placeholder: '例如：赶集网'
+                }] // Todo: add multi checkbox support
+            },
+            account: {
+                modalTitle: '添加新账户',
+                formFields: [{
+                    label: '豌豆荚账户',
+                    key: 'wdjAccount'
+                }, {
+                    label: '账户角色',
+                    key: 'role',
+                    optionStr: 'idx as opt for (idx, opt) in options.options',
+                    options: ['内部运营', '外部运营', '厂商工作人员']
+                }, {
+                    label: '可管理的 OEM/API',
+                    controlTpl: '' // 异步接口数据，自定义 checkbox
+                }, {
+                    label: '开设权限',
+                    controlTpl: ''
+                }]
+            }
         };
 
-        var apiPartnerForm = {
-            modalTitle: '添加 API 合作方',
-            formFields: [{
-                label: 'API 合作方名称',
-                key: 'hzfcmc1',
-                placeholder: '例如：赶集网'
-            }, {
-                label: 'API 合作方授权 ID',
-                key: 'hzfcmc2',
-                placeholder: '例如：赶集网'
-            }, {
-                label: 'API 方下载豌豆荚 APK  包的 URL (可选)',
-                key: 'hzfcmc3',
-                placeholder: '用于追踪 API 合作方为豌豆荚这款应用带来的装机量'
-            }, {
-                label: '优质应用变更时通知合作方的 API 地址（可选）',
-                key: 'hzfcmc4',
-                placeholder: '由合作方提供，在优质应用变更时调用'
-            }, {
-                label: '接口访问频率权限',
-                key: 'hzfcmc5',
-                optionStr: 'idx as opt for (idx, opt) in options.options',
-                options: ['高级授权', '中级授权', '低级授权']
-            }, {
-                label: '开放的接口',
-                key: 'hzfcmc',
-                placeholder: '例如：赶集网'
-            }] // Todo: add multi checkbox support
+        // Todo: 为什么 child 访问不到？！
+        // 离开的时候，恢复$root
+        $scope.$root.onEdit = function(type, item) {
+            console.log(arguments);
+            formlyHelper.openModal(FORMMAP[type]);
+        };
+        $scope.$root.onAdd = function(type, item) {
+            formlyHelper.openModal(FORMMAP[type]);
+        };
+        $scope.$root.onDel = function(type, item) {
+            if (window.confirm('您确定要删除该资源吗？')) {
+                apiHelper('del' + type).then(function() {
+
+                });
+            }
         };
 
         // Todo: with formName: <账户>, type: add/modify to compute modalTitle
         // Todo: 根据 role 的取值，需要切换 formFields 内容！ - accountPartnerForm
-        var accountForm = {
-            modalTitle: '添加新账户',
-            formFields: [{
-                label: '豌豆荚账户',
-                key: 'wdjAccount'
-            }, {
-                label: '账户角色',
-                key: 'role',
-                optionStr: 'idx as opt for (idx, opt) in options.options',
-                options: ['内部运营', '外部运营', '厂商工作人员']
-            }, {
-                label: '可管理的 OEM/API',
-                controlTpl: '' // 异步接口数据，自定义 checkbox
-            }, {
-                label: '开设权限',
-                controlTpl: ''
-            }]
-        };
 
-        formlyHelper.openModal(apiPartnerForm);
+        // formlyHelper.openModal(apiPartnerForm);
     });
 
-    systemApp.controller('systemPartnerCtrl', function($scope) {
+    systemApp.controller('systemPartnerCtrl', function($scope, apiHelper) {
+        apiHelper('fetchCandidateType', {
+            busy: 'global'
+        }).then(function(r) {
+            $scope.$root.meta = r;
+        });
         // pickup from resp.data with specfic fields
         // add raw item for each row, which include other info which not to show
-        $scope.tableHead = ['合作方名称', '合作方 ID', '接口访问次数限制', '操作'];
-        $scope.tableData = [
-            ['联想', 'Lneno', '3'],
-            ['联想', 'Lneno', '3']
-        ];
-        wrapTable($scope.tableData, {
-            editable: true,
-            deletable: true,
-            type: 'partner'
+        apiHelper('fetchApiPartners', {
+            busy: 'global'
+        }).then(function(r) {
+            $scope.apiPartnerList = r.beans;
         });
-
-        // Todo: 为什么 child 访问不到？！
-        // 离开的时候，恢复$root
-        $scope.$root.onEdit = function(type, row) {
-            // row.raw - default item resource data
-            console.log(arguments);
-        };
-        $scope.onDel = function(type, row) {
-            // row.raw - default item resource data
-            console.log(arguments);
-        };
+        apiHelper('fetchOemPartners', {
+            busy: 'global'
+        }).then(function(r) {
+            $scope.oemPartnerList = r.beans;
+        });
     });
 
     systemApp.controller('systemAccountCtrl', function($scope) {
